d580b37b352aee6cf3ae30b6b48bccefbc36c808
Prajwal P <prajwalp1106@gmail.com>
2024-09-17 20:56:33 +0530

    minor changes

--- src/services/api.js
+++ src/services/api.js
@@ -1,6 +1,6 @@
 import axios from 'axios';
 
-const API_URL = 'http://localhost:8080';
+const API_URL = 'https://nitte-placement-resources.onrender.com';
 
 export const savePost = async (payload) => {
     try {
